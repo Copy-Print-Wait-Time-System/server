e3b253903109c17ac75585f54de8f4469489a7de
Tommy-Las <tommy_las@hotmail.com>
2023-02-12 13:20:20 -0500

    minor changes added

--- dist/functions/sortByDistance.js
+++ dist/functions/sortByDistance.js
@@ -3,11 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.zipcodeFilter = void 0;
+exports.sortByDistance = void 0;
 const zipcodes_1 = __importDefault(require("zipcodes"));
 //import zipcode, { near } from 'zipcodes-nearby';
-function zipcodeFilter(zipcode, radius) {
-    const radiusArray = zipcodes_1.default.radius(zipcode, radius);
-    return radiusArray;
+function sortByDistance(zipcode, zipcodeArray) {
+    const sortedArray = [];
+    for (let i = 0; i < zipcodeArray.length; i++) {
+        var dist = zipcodes_1.default.distance(zipcode, zipcodeArray[i]); //In Miles
+    }
+    return sortedArray;
 }
-exports.zipcodeFilter = zipcodeFilter;
+exports.sortByDistance = sortByDistance;
