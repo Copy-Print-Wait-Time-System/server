24a2cf3a4c7f2c3913f522fe2868c472c6721e34
Alejandro Carvajal <alex_carvajal95@hotmail.com>
2023-02-26 22:37:15 -0500

    Working on editCustomer.

--- src/controllers/editCustomer.controller.ts
+++ src/controllers/editCustomer.controller.ts
@@ -6,9 +6,27 @@ export function editCustomer (req: Request, res: Response){
     const connection = connect();
     const data = req.body
     const store_id = req.params.store_id
+    const userID = req.body.userID
 
     console.log(data)
 
-    return res.status(201).send("Customer successfully edited from the queue in store #" + store_id)
+    
+    connection.query(`Select * from queues where userID = ${userID};`, (err:any, result:any) => {
+
+        
+        //Check if queue is empty
+        if (result[0] == null){
+            return res.status(201).send(`There is no user with ID ${userID}.`)
+        }
+
+        //dbPosition comes back as a JSON, this code gets the value as a string
+        var json = JSON.parse(JSON.stringify(result[0]));
+        var userID = json["userID"]
+
+        
+
+
+        return res.status(201).send("Customer successfully edited from the queue in store #" + store_id)
+    })
 
 }
