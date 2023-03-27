3dcbd6dc67102cc5cca37db632c43078472edb88
Alejandro Carvajal <alex_carvajal95@hotmail.com>
2023-03-26 21:58:52 -0400

    Added authenticate function.

--- src/functions/passwordEncrypt.ts
+++ src/functions/passwordEncrypt.ts
@@ -1,8 +1,25 @@
+import { connect } from "../database";
+
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-export function encrypt(password: string, color: boolean) : string {
+export function encrypt(password: string) : string {
 
- return bcrypt.hash(password, saltRounds);
+    return bcrypt.hash(password, saltRounds);
+
+}
+
+export function authenticate(storeID: number, password: string) : boolean {
+
+    const connection = connect();
+    
+    var storePW = connection.query(`select hashedPW from storepasswords where store = ${storeID};`, (err:any, dbPW:any) => {
+        var json = JSON.parse(JSON.stringify(dbPW[0]));
+        var storePW = json["hashedPW"];
+
+        return storePW;
+    });
+
+    return bcrypt.compare(password, storePW);
 
 }
