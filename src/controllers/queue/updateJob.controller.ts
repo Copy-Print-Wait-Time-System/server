5d760309b04f4996661d5337660a0752405dcfac
Tommy-Las <tommy_las@hotmail.com>
2023-04-19 22:41:04 -0400

    fix for updateJob

--- src/controllers/queue/updateJob.controller.ts
+++ src/controllers/queue/updateJob.controller.ts
@@ -21,24 +21,22 @@ export function updateJob (req: Request, res: Response){
         const difference = customerTime - estTime;
 
         // Updates all the fields for a user in the database
-        connection.query(`UPDATE queues SET estimatedWaitTime = estimatedWaitTime - ${difference} WHERE store = ${store_id} AND position > ${position};UPDATE queues Q, customerJobs C, customerJobsOptional CO
+        connection.query(`UPDATE queues SET estimatedWaitTime = estimatedWaitTime - ${difference} WHERE store = ${store_id} AND position > ${position}; UPDATE queues Q, customerJobs C, customerJobsOptional CO
         SET Q.firstName = "${data.fname}", Q.lastName = "${data.lname}", Q.estimatedWaitTime = ${estTime},
         C.job = "${data.job}", C.copies = ${data.copies}, C.numPages = ${data.numPages}, C.paperSize = "${data.paperSize}", C.paperType = "${data.paperType}", C.fitPaper = "${data.fitPaper}", C.color = "${data.color}", C.sides = "${data.sides}", C.orientation = "${data.orientation}", C.jobCollate = "${data.collate}",
         CO.stapling = "${data.stapling}", CO.cutting = "${data.cutting}", CO.folding = "${data.folding}", CO.holePunching = "${data.holePunching}", CO.waferSealColor = "${data.waferSealColor}", CO.waferSealSides = "${data.waferSealSides}", CO.perforation = "${data.perforation}", CO.lamination = "${data.lamination}", CO.shrinkWrap = "${data.shrinkWrap}", CO.addFoamBoardMounting = "${data.addFoamBoardMounting}", CO. removePages = "${data.removePages}", CO.slipsheet = "${data.slipsheet}", CO.trimToEdge = "${data.trimToEdge}", CO.specialInstructions = "${data.specialInstructions}"
         WHERE Q.userID = ${data.userID} AND Q.userID = C.userID AND Q.userID = CO.userID;`, async (err:any, result:any) => {
-        if (err){
-            console.error(err);
-            return res.status(400).send("Error updating values.");
-        }
+        
+            if (err){
+                console.error(err);
+                return res.status(400).send("Error updating values.");
+            }
 
-        await updateStoreWaitTime(store_id);
+            await updateStoreWaitTime(store_id);
 
-        return res.status(201).send("Customer updated successfully.");
+            return res.status(201).send("Customer updated successfully.");
+
+        });
     });
-    });
 
-    
-    
-
-    return res.status(201).send("TESTING");
 }
