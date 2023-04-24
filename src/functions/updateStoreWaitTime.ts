ec24a2d1a43599cae6398874c9bc08d6dd728a8e
Tommy-Las <tommy_las@hotmail.com>
2023-04-23 23:47:36 -0400

    when removing customer from front, update store wait time

--- src/functions/updateStoreWaitTime.ts
+++ src/functions/updateStoreWaitTime.ts
@@ -5,7 +5,7 @@ export function updateStoreWaitTime(store_id: string) : Promise<string> {
     const connection = connect();
 
     return new Promise<string>((resolve, reject) => {
-        connection.query(`UPDATE stores SET waitTime = (SELECT sum(customerTime)
+        connection.query(`UPDATE stores SET waitTime = (SELECT max(estimatedWaitTime)
         FROM queues
         WHERE store = ${store_id}) 
         WHERE stores.store = ${store_id};`, (err:any, res:any) => {
