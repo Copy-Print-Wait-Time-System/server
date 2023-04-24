b93fd2ba6ad2b3823a19259e6c1bf5d7113a1f9c
Tommy-Las <tommy_las@hotmail.com>
2023-04-24 19:10:48 -0400

    removeCustomer should update correctly

--- src/controllers/store/removeCustomer.controller.ts
+++ src/controllers/store/removeCustomer.controller.ts
@@ -10,12 +10,12 @@ export function removeCustomer (req: Request, res: Response){
 
     const userPosition = data.position;
     const user_ID = data.userID;
-    const estimatedWaitTime = data.estimatedWaitTime;
+    const customerTime = data.customerTime;
    
         // First query updates the estimatedWaitTime for affected customers
         //second query updates the positions of all users behind the removed user
         //Last query uses the userID to remove the user from the queue
-        connection.query(`UPDATE queues SET estimatedWaitTime = estimatedWaitTime - ${estimatedWaitTime} WHERE store = ${store_id} AND position > ${userPosition};UPDATE queues SET position = position - 1 WHERE store = ${store_id} AND position > ${userPosition};DELETE FROM queues WHERE userID = ${user_ID};`, async (err:any, result:any) => {
+        connection.query(`UPDATE queues SET estimatedWaitTime = estimatedWaitTime - ${customerTime} WHERE store = ${store_id} AND position > ${userPosition};UPDATE queues SET position = position - 1 WHERE store = ${store_id} AND position > ${userPosition};DELETE FROM queues WHERE userID = ${user_ID};`, async (err:any, result:any) => {
             await updateStoreWaitTime(store_id);
             
             connection.end()
